9b6f46e8b371f0758c93d51af455ef8d024b9994
hamon1 <leejh021122@naver.com>
2024-12-14 01:13:08 +0900

    041 - 수정

--- 정수론/Code_041.java
+++ 정수론/Code_041.java
@@ -16,15 +16,18 @@ public class Code_041 {
         long n = Long.parseLong(br.readLine());
 
         long result = n;
-        for (long p = 2; p < Math.sqrt(n); p++) {
+        // n = p_1^x * p_2^Y * ...;
+        // p 는 소수
+        for (long p = 2; p <= Math.sqrt(n); p++) {
             if (n % p == 0) {
                 result = result - result/p;
-                while (n % p == 0) {
+                while (n % p == 0) { // p_1^x 에서 x만큼 나누기 (해당 소수를 식에서 완전 삭제)
                     n /= p;
                 }
+                // 후에 n 안의 새로운 소수를 찾음
             }
         }
-        if (n > 1) {
+        if (n > 1) { // n이 1 이상인 경우 == n의 식 안에 포함될 숫자가 남음
             result = result - result/n;
         }
         System.out.println(result);
